7513cae3672f6069c75816f8814878493c527d66
Roheena Sadiq <95050380+roh210@users.noreply.github.com>
2026-07-08 09:45:11 +0100

    Add: 4sum - submission-1

---
+++ Data Structures & Algorithms/4sum/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[][]}
+     */
+    fourSum(nums, target) {
+        const res = [];
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+            for (let j = i + 1; j < nums.length - 2; j++) {
+                if (j > i+1 && nums[j] === nums[j - 1]) continue;
+
+                let l = j + 1,
+                    r = nums.length - 1;
+                while (l < r) {
+                    let sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum < target) {
+                        l++;
+                    } else if (sum > target) {
+                        r--;
+                    } else {
+                        res.push([nums[i], nums[j], nums[l], nums[r]]);
+                        l++;
+                        while (nums[l] === nums[l - 1] && l < r) l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
