5acd10c04012cffb7e005b33534fd69339f5cc14
Roheena Sadiq <95050380+roh210@users.noreply.github.com>
2026-07-11 13:07:35 +0100

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        people.sort((a, b) => a - b);
+        let l = 0,
+            r = people.length - 1;
+        let res = 0;
+        while (l <= r) {
+            if (people[l] + people[r] > limit) {
+                let maxVal = Math.max(people[l], people[r]);
+                res +=1;
+                r--;
+            } else {
+                res+=1;
+                l++;
+                r--;
+            }
+        }
+        return res;
+    }
+}
