24bc382f484a431d9a58fc636f785a3e49a0f9a1
Roheena Sadiq <95050380+roh210@users.noreply.github.com>
2026-08-28 12:44:27 +0100

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {};
+        for (let s of strs) {
+            const count = new Array(26).fill(0);
+            for (let c of s) {
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+            }
+            if (!res[count]) {
+                res[count] = [];
+            }
+            res[count].push(s);
+        }
+        return Object.values(res);
+    }
+}
